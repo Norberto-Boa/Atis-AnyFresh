30b189017cd18edbdfb949046de2f178a4f67ddb
Norberto Boa <nboa.26@gmail.com>
2024-10-10 22:24:07 +0200

    ADD: docker db

--- server/src/useCases/createExpense/createExpenseController.ts
+++ server/src/useCases/createExpense/createExpenseController.ts
@@ -1,49 +1,49 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import { z } from "zod";
 import { checkIfValidUUID } from "../../utils/checkIfValidUUID";
 import { createExpenseUseCase } from "./createExpenseUseCase";
 
+class createExpenseController {
+	async create(req: Request, res: Response) {
+		const { user } = req.headers;
 
-class createExpenseController{
-  async create(req: Request, res: Response) {
-    const { user } = req.headers;
-    
-    const createExpenseBody = z.object({
-      description: z
-        .string({ required_error: "A descricao deve ser um texto!" })
-        .min(2, { message: "A descricao deve ter pelo menos 2 caracteres!" }),
-      buyerName: z
-        .string({ required_error: "O nome do comprador deve ser um texto!" })
-        .min(2, { message: "O nome do comprador deve ter pelo menos 2 caracteres!" }),
-      price: z
-        .number({ required_error: "O preco deve ser um numero!" }),
-      quantity: z
-        .number({ required_error: "A quantidade deve ser um numero!" })
-        .min(1, { message: "Nao pode registar um gasto com quantidade 0." }),
-      date: z
-        .string({ required_error: "A data nao esta bem formatada!" })
-    }).parse(req.body);
+		const createExpenseBody = z
+			.object({
+				description: z
+					.string({ required_error: "A descricao deve ser um texto!" })
+					.min(2, { message: "A descricao deve ter pelo menos 2 caracteres!" }),
+				buyerName: z
+					.string({ required_error: "O nome do comprador deve ser um texto!" })
+					.min(2, {
+						message: "O nome do comprador deve ter pelo menos 2 caracteres!",
+					}),
+				price: z.number({ required_error: "O preco deve ser um numero!" }),
+				quantity: z
+					.number({ required_error: "A quantidade deve ser um numero!" })
+					.min(1, { message: "Nao pode registar um gasto com quantidade 0." }),
+				date: z.string({ required_error: "A data nao esta bem formatada!" }),
+			})
+			.parse(req.body);
 
-    if (typeof user !="string") {
-      throw new Error("O id inserido é inválido!");
-    };
+		if (typeof user !== "string") {
+			throw new Error("O id inserido é inválido!");
+		}
 
-    const { description, buyerName, date, price, quantity } = createExpenseBody;
-    
-    const createExpense = new createExpenseUseCase();
+		const { description, buyerName, date, price, quantity } = createExpenseBody;
 
-    const expense = await createExpense.handle({
-      userId: user,
-      description,
-      date,
-      buyerName,
-      price,
-      quantity
-    });
+		const createExpense = new createExpenseUseCase();
 
-    return res.status(200).json({ expense });
+		const expense = await createExpense.handle({
+			userId: user,
+			description,
+			date,
+			buyerName,
+			price,
+			quantity,
+		});
 
-  }
+		return res.status(200).json({ expense });
+	}
 }
 
 export { createExpenseController };
