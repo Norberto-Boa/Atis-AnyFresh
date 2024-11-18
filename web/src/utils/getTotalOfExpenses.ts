ff31e39a08cd6272db3173db164c9c701571b014
Norberto Boa <nboa.26@gmail.com>
2024-11-18 12:05:43 +0200

    Fix error on get expenses total function

--- web/src/utils/getTotalOfExpenses.ts
+++ web/src/utils/getTotalOfExpenses.ts
@@ -1,8 +1,10 @@
-export function getTotalOfExpenses(expenses: []): number {
-  let totalExpenses = 0;
-  expenses.forEach((expense: { quantity: number; price: number }) => {
-    totalExpenses += expense.quantity * expense.price;
-  });
+import type { expense } from "@/types/_types";
 
-  return totalExpenses;
+export function getTotalOfExpenses(expenses: expense[] | 0): number {
+	let totalExpenses = 0;
+	expenses.forEach((expense: { quantity: number; price: number }) => {
+		totalExpenses += expense.quantity * expense.price;
+	});
+
+	return totalExpenses;
 }
