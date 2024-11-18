2338e588a7c1a7cc9a84d36d0aa443a7bb0fa76f
Norberto Boa <nboa.26@gmail.com>
2024-11-18 11:51:06 +0200

    Changes on front end!

--- web/src/services/getApiClient.ts
+++ web/src/services/getApiClient.ts
@@ -1,27 +1,27 @@
 import { baseUrl } from "@/utils/axios";
 import axios from "axios";
 import { parseCookies } from "nookies";
-import {verify} from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 import type { decodedToken } from "@/utils/checkJwt";
 
 function getAPIclient(ctx?: any) {
-  const { 'atis.token': token } = parseCookies(ctx);
-  
-  const api = axios.create({
-    baseURL: baseUrl,
-  })
-  
-  api.interceptors.request.use(config => {
-    return config;
-  })
-  
-  if (token) {
-    // const decodedToken  = verify(token, "Mena")
-    api.defaults.headers['Authorization'] = 'Bearer ' + token;
-    // api.defaults.headers.post['user']= decodedToken.sub?.toString();
-  }
+	const { "atis.token": token } = parseCookies(ctx);
 
-  return api;
+	const api = axios.create({
+		baseURL: baseUrl,
+	});
+
+	api.interceptors.request.use((config) => {
+		return config;
+	});
+
+	if (token) {
+		// const decodedToken  = verify(token, "Mena")
+		api.defaults.headers["Authorization"] = `Bearer ${token}`;
+		// api.defaults.headers.post['user']= decodedToken.sub?.toString();
+	}
+
+	return api;
 }
 
 export { getAPIclient };
