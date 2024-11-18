bef514aa69b9c9dd1ccc0a4c3b83bb3077d0d37d
Norberto Boa <nboa.26@gmail.com>
2024-11-18 11:53:52 +0200

    Fix types error on Input component

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,19 +1,19 @@
-import { IExpenseCreate, IProductCreate } from "@/types/inputTypes";
-import { InputHTMLAttributes } from "react";
-import { Path, UseFormRegister } from "react-hook-form";
+import { type Expense, IProductCreate } from "@/types/inputTypes";
+import type { InputHTMLAttributes } from "react";
+import type { Path, UseFormRegister } from "react-hook-form";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: Path<IExpenseCreate>;
-  register: UseFormRegister<IExpenseCreate>;
+	label: Path<Expense>;
+	register: UseFormRegister<Expense>;
 }
 
 const Input = (props: InputProps) => {
-  return (
-    <input
-      {...props.register(props.label)}
-      className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 w-[100%] mt-1"
-    />
-  );
+	return (
+		<input
+			{...props.register(props.label)}
+			className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 w-[100%] mt-1"
+		/>
+	);
 };
 
 export { Input };
